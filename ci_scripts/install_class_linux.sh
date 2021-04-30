188910241f84fc6f7a4569cd7355c19b6e5142f0
Tim Morton <tim.morton@gmail.com>
2021-04-30 11:46:26 -0700

    add workflow

---
+++ ci_scripts/install_class_linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/veragluscevic/class_public.git
+cd class_public
+git checkout dmb
+make -j4
+
+# at this point the make file leaves you in the python dir
+cd ..
