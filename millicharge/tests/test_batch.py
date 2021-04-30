0ecac1dae117c279ae19b9d9f78825de04671d90
Tim Morton <tim.morton@gmail.com>
2021-04-30 15:44:06 -0700

    add full sim run tests

--- millicharge/tests/test_batch.py
+++ millicharge/tests/test_batch.py
@@ -2,9 +2,22 @@ from pkg_resources import resource_filename
 
 from millicharge.batch import SimGroup
 
+test_file = resource_filename('millicharge', 'tests/test.yaml')
 
 def test_initialize():
-    test_file = resource_filename('millicharge', 'tests/test.yaml')
     sims = SimGroup(test_file)
     sims.test()
     
+
+def test_sims():
+    sims = SimGroup(test_file)
+
+    from multiprocessing import Pool
+
+    pool = Pool(3)
+    sims.run(pool=pool)
+
+    plot = sims.global_signature()
+
+    for name in sims.analysis.keys():
+        assert sims.history_df(name).isnull().sum().sum() == 0
